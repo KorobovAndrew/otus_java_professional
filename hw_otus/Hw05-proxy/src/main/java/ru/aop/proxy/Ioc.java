d03202aad23a203b87d97cd3be098f29e9da2b94
AndrewBox <AndrewBoxMail@yandex.ru>
2023-08-17 22:28:30 +0300

    hw05 to review

--- hw_otus/Hw05-proxy/src/main/java/ru/aop/proxy/Ioc.java
+++ hw_otus/Hw05-proxy/src/main/java/ru/aop/proxy/Ioc.java
@@ -6,7 +6,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Ioc {
 
@@ -25,26 +26,23 @@ public class Ioc {
 
     static class TestLoggingInvocationHandler implements InvocationHandler {
         private final TestLogging testLogging;
-        private final List<Method> loggingMethods;
+        private final Map<String, Object[]> loggingMethods;
 
         TestLoggingInvocationHandler(TestLogging testLogging) {
             this.testLogging = testLogging;
-            this.loggingMethods = Arrays.stream(testLogging
-                            .getClass()
-                            .getDeclaredMethods())
+            this.loggingMethods = Arrays.stream(testLogging.getClass().getDeclaredMethods())
                     .filter(method -> method.isAnnotationPresent(Log.class))
-                    .toList();
+                    .collect(Collectors.toMap(
+                            Method::getName,
+                            Method::getParameterTypes));
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            loggingMethods.stream()
-                    .filter(m -> m.getName().equals(method.getName()))
-                    .filter(m -> Arrays.equals(m.getParameterTypes(), method.getParameterTypes()))
-                    .findAny()
-                    .ifPresent(m -> System.out.printf("executed method: %s, params: %s\n",
-                            m.getName(),
-                            Arrays.toString(args)));
+            var methodName = method.getName();
+            if (loggingMethods.containsKey(methodName)
+                    && Arrays.equals(loggingMethods.get(methodName), method.getParameterTypes()))
+                System.out.printf("executed method: %s, params: %s\n", methodName, Arrays.toString(args));
             return method.invoke(testLogging, args);
         }
     }
